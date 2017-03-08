d4a151f6c74a555484a3af0d235a55495120f3cb
SamGomes <sam.s.gomes@sapo.pt>
2017-03-08 23:38:32 +0000

    Triangles done. Still inefficient though...

--- exercise1/P3DExercise1/scene.cpp
+++ exercise1/P3DExercise1/scene.cpp
@@ -4,6 +4,8 @@
 
 #include "sphere.h"
 #include "plane.h"
+#include "triangle.h"
+
 
 #include "scene.h"
 
@@ -222,6 +224,43 @@ bool Scene::loadSceneFromNFF(char * path)
 			newPlane->setMaterial(currentMaterial);
 			this->objects->push_back(newPlane);
 		}
+
+		//check p for triangulized polygon
+		else if (line.substr(0, 2) == "p ") {
+
+			int totalVertices = std::stoi(line.substr(2, 3));
+
+			for (int i = 0; i < totalVertices; i+=3) {
+				std::string insideLine;
+				std::getline(fileStream, insideLine);
+
+				std::istringstream triangleStream(insideLine);
+				glm::vec3 trianglePosition1, trianglePosition2, trianglePosition3;
+
+				triangleStream >> x; triangleStream >> y; triangleStream >> z;
+				trianglePosition1 = glm::vec3(x, y, z);
+
+				std::getline(fileStream, insideLine);
+				triangleStream = std::istringstream(insideLine);
+				
+				triangleStream >> x; triangleStream >> y; triangleStream >> z;
+				trianglePosition2 = glm::vec3(x, y, z);
+
+				std::getline(fileStream, insideLine);
+				triangleStream = std::istringstream(insideLine);
+
+				triangleStream >> x; triangleStream >> y; triangleStream >> z;
+				trianglePosition3 = glm::vec3(x, y, z);
+
+				Triangle * newTriangle = new Triangle(trianglePosition1, trianglePosition2, trianglePosition3);
+				newTriangle->setMaterial(currentMaterial);
+				this->objects->push_back(newTriangle);
+				
+			}
+
+
+		}
+
 	}
 
 	return true;
---
+++ exercise1/P3DExercise1/triangle.h
@@ -0,0 +1,32 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include"object.h"
+
+class Triangle : public Object {
+
+private:
+	glm::vec3 position2;
+	glm::vec3 position3;
+	glm::vec3 normal;
+
+	glm::vec3 v12;
+	glm::vec3 v31;
+	glm::vec3 v23;
+
+	float d;
+
+public:
+	Triangle(glm::vec3 position1, glm::vec3 position2, glm::vec3 position3);
+
+	glm::vec3 getPosition2();
+	glm::vec3 getPosition3();
+
+	bool hasIntersection(Ray ray);
+	bool getIntersectionPoint(glm::vec3& intersect, Ray ray);
+	glm::vec3 getNormal(glm::vec3 intersectionPoint, Ray ray);
+	bool isInside(glm::vec3 point);
+};
+
+
+#endif;
