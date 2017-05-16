d777468ba3cc2d479ded68c7fb9132cd30c47266
Tiago <tiagoalmeida.92@hotmail.com>
2017-05-16 19:57:14 +0100

    Added first person character to obstacleScene.

---
+++ assignment3/Assignment3/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+
+public class MoveScript : MonoBehaviour
+{
+    public int speed;
+    
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+    }
+
+}
