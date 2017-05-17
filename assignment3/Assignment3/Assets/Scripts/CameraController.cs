b48fc77fa0d370f9adcc29cdf0c4cfbf4b5457f4
SamGomes <sam.s.gomes@sapo.pt>
2017-05-17 20:32:50 +0100

    Added map and some collisions

---
+++ assignment3/Assignment3/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position -  player.transform.position;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        Vector3 forward = new Vector3(0, 0, 1);
+        transform.rotation = player.transform.rotation;
+        transform.position = player.transform.position + offset;
+
+        
+    }
+}
--- assignment3/Assignment3/Assets/Scripts/MoveScript.cs
+++ assignment3/Assignment3/Assets/Scripts/MoveScript.cs
@@ -4,13 +4,19 @@ using System;
 public class MoveScript : MonoBehaviour
 {
     public int speed;
+    public GameObject playerMesh;
     
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        playerMesh.transform.position = transform.position;
+        playerMesh.transform.Rotate(0, Input.GetAxis("Mouse X") * 2, 0);
+        float yComponent = Mathf.Deg2Rad * playerMesh.transform.rotation.eulerAngles.y;
+        Vector3 movement = new Vector3(moveVertical * Mathf.Sin(yComponent) + moveHorizontal * Mathf.Cos(yComponent), 0.0f, moveVertical * Mathf.Cos(yComponent) + moveHorizontal * Mathf.Sin(yComponent));
+
+        //movement.y= movement.magnitude* Mathf.Sin(Time.deltaTime);
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
     }
