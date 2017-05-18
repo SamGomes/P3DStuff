f9db55d09d08cf26044956d1757dfcdaae272862
unknown <afcfonseca93@gmail.com>
2017-05-18 20:29:54 +0100

    Head up display on the way...

---
+++ assignment3/Assignment3/Assets/HeadsUp/RotatePlayerArrow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotatePlayerArrow : MonoBehaviour {
+
+    public Camera fromCamera = null;
+    public Image toImage = null;
+
+	// Use this for initialization
+	void Update () {
+        Debug.Log("lalal");
+        if(fromCamera != null && toImage != null)
+            toImage.rectTransform.rotation.Set(0,0, fromCamera.transform.rotation.y,1);	
+	}
+}
