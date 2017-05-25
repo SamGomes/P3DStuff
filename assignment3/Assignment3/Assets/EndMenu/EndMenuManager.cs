c6a077d36949ada493d116aa0b6691830d36cac0
unknown <afcfonseca93@gmail.com>
2017-05-25 02:11:12 +0100

    End scene UI in place. Code not working.

---
+++ assignment3/Assignment3/Assets/EndMenu/EndMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndMenuManager : MonoBehaviour {
+    public Image newHighscorePanel;
+    public Image topHighscorePanel;
+
+    // Use this for initialization
+    public void changePanels() {
+        newHighscorePanel.GetComponent<Animator>().SetBool("move", true);
+        topHighscorePanel.GetComponent<Animator>().SetBool("move", true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
