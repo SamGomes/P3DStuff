98e828dd5099aae8e90ad1d5a37a29300fc36665
unknown <afcfonseca93@gmail.com>
2017-05-27 23:35:00 +0100

    Doors now check if closed and can activate a secret!

--- assignment3/Assignment3/Assets/Scripts/Door.cs
+++ assignment3/Assignment3/Assets/Scripts/Door.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Door : MonoBehaviour {
+    public bool isSecretDoor;
+    private bool isClosed = false;
 
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.GetComponent<Player>())
+        if (col.gameObject.GetComponent<Player>() && !isClosed)
         {
+            isClosed = true;
+            if (isSecretDoor) {
+                GameObject.Find("ScoreController").GetComponent<ScoreController>().addScore("Secret!!!", 5000);
+            }
             this.GetComponent<AudioSource>().Play();
             this.GetComponent<Animator>().SetBool("closeDoor", true);
         }
