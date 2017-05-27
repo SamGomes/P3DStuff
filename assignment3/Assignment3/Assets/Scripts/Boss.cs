9df43ada2cbe4ec67e31ed220c3e90ca088b1d12
SamGomes <sam.s.gomes@sapo.pt>
2017-05-27 23:31:45 +0100

    boss started

---
+++ assignment3/Assignment3/Assets/Scripts/Boss.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : Enemy
+{
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (this.isDead)
+            return;
+
+        if (life <= 0)
+        {
+            this.GetComponent<Animator>().SetBool("bossKill", true);
+        }
+
+
+    }
+
+}
