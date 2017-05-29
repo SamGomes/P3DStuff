5999d70c117f02d46f5aa15858b3001e37339bf2
unknown <afcfonseca93@gmail.com>
2017-05-29 19:36:34 +0100

    Boss death stuff

---
+++ assignment3/Assignment3/Assets/gameComponents/boss/Boss.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        playSound();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void playSound() {
+        GetComponent<AudioSource>().Play();
+    }
+
+    void afterBossDeath() {
+
+    }
+}
