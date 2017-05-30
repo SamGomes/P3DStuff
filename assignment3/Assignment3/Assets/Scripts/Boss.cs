01a007d67d593c2bd8e562d1169c7e460089f779
unknown <afcfonseca93@gmail.com>
2017-05-30 22:17:52 +0100

    Executable and changes in the README.

---
+++ assignment3/Assignment3/Assets/Scripts/Boss.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : Enemy {
+
+	// Use this for initialization
+	void Start () {
+        base.initialize();
+        playSound();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void playSound() {
+        GetComponent<AudioSource>().Play();
+    }
+
+    public void afterBossDeath() {
+        ScoreController scoreController = GameObject.Find("ScoreController").GetComponent<ScoreController>();
+        scoreController.addScore("Boss Kill", 10000);
+        int timeBonus = 240 - (int)Time.timeSinceLevelLoad;
+        if (timeBonus > 0) {
+            scoreController.addScore("Time x" + timeBonus, timeBonus * 250);
+        }
+
+        StartCoroutine(GameObject.Find("room").GetComponent<MapEvents>().endGame());
+    }
+
+    override public void injure(int hp, GunType gun, Collider collider)
+    {
+        if (this.isDead)
+            return;
+
+        base.injure(hp, gun, collider);
+
+        if (isDead)
+        {
+            transform.GetChild(0).GetComponent<Animator>().SetBool("isDead", true);
+            GetComponent<Animator>().speed = 0;
+        }
+        else {
+            transform.GetChild(0).GetComponent<Animator>().SetTrigger("isHurt");
+        }
+    }
+}
--- assignment3/README.txt
+++ assignment3/README.txt
@@ -3,12 +3,14 @@
 //////////////////////////////////////
 
 
-Para correr o jogo pode ser utilizado o executável Doom.exe
-Ou pode ser corrido o StartMenu scene no unity, desenvolvido na versão 5.6.1.
+Para correr o jogo, pode ser carregada a pasta de projecto "Assignment3" no unity e depois correr a cena StartMenu scene , desenvolvido na versão 5.6.1.
+Ou então, na pasta "Binary", carregando no executável "Doom^2".
+
+O jogo deve ser corrido na resulução 1024×768.
 
 Os controlos do player são:
 
-WASD - Movimento de jogador
+W,A, S e D - Movimento de jogador
 Space - Saltar
 Shift - Correr
 Botão direito do rato - Disparar armas
